594695e981071574086238b3c50d9e8313e17500
bala-amypo <bala@amypo.in>
2025-12-17 08:26:03 +0000

    User commit

--- src/main/java/com/example/demo/Repository/StudentRepository.java
+++ src/main/java/com/example/demo/Repository/StudentRepository.java
@@ -1,4 +1,4 @@
-package com.example.demo.Repository;
+package com.example.demo.repository;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.data.jpa.repository;
 import org.springframework.sterotype.repository;
